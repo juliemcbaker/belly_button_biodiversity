67067eb3349549bf211780d16c82a021677e35d4
juliemcbaker <83370545+juliemcbaker@users.noreply.github.com>
2021-08-19 15:33:59 -0500

    trying to build plotly code despite no data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,18 @@ d3.json("samples.json").then((data)=> {
     dropdown=d3.select("#selDataset")
     console.log(data);
 });
+
+// basic horizontal bar chart
+var data = [{
+    type: 'bar',
+    x: [sample_values],
+    y: [otu_ids],
+    orientation: 'h'
+    text: [otu_labels]
+  }];
+
+  // hover labels, but this only comes listed with line & scatterplots
+var layout = {title: 'Hover over the points to see the text'};
+Plotly.newPlot('myDiv', data, layout);  
+Plotly.newPlot('myDiv', data);
+
