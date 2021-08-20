0346fe6400be0c2b0b090637855c39a0658b1885
juliemcbaker <83370545+juliemcbaker@users.noreply.github.com>
2021-08-20 13:44:08 -0500

    bubble chart works

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -15,78 +15,48 @@ function create_charts(choose_data){
         user_choice = user_choice[0]    
         console.log(user_choice)
     
-    // basic horizontal bar chart
+    // basic horizontal bar chart; reverse makes largest value appear at top of graph
     var hbar_data = [{
         type: 'bar',
-        x: [sample_values],
-        y: [otu_ids],
+        x: user_choice.sample_values.slice(0,10).reverse(), 
+        y: user_choice.otu_ids.map(el => "OTU " + el).slice(0,10).reverse(),
         orientation: 'h',
-        text: [otu_labels]
-//   }];    
-    })
+        text: user_choice.otu_labels.slice(0,10).reverse()
+      }
+    ];    
+    var barlayout={
+        title: (`Top Ten OTUs for ID ${user_choice.id}`)
+      }
+    Plotly.newPlot('bar', hbar_data, barlayout)
+    
+    //bubble chart
+    var bubble_data = [
+      {
+        x: user_choice.otu_ids,
+        y: user_choice.sample_values,
+        text: user_choice.otu_labels,
+        mode: "markers",
+        marker: {
+            color: user_choice.otu_ids,
+            size: user_choice.sample_values,
+            text: user_choice.otu_labels
+        }
+      }
+    ];
+    var bubble_layout={
+      title: (`All OTUs for ID ${user_choice.id}`)
+    }
+    Plotly.newPlot("bubble", bubble_data, bubble_layout)
+  })
 }
 
 
-// basic horizontal bar chart
-// var data = [{
-//     type: 'bar',
-//     x: [sample_values],
-//     y: [otu_ids],
-//     orientation: 'h',
-//     text: [otu_labels]
-//   }];
-
-  // hover labels, but this only comes listed with line & scatterplots
-// var layout = {title: 'Hover over the points to see the text'};
-// Plotly.newPlot('myDiv', data, layout);  
-// Plotly.newPlot('myDiv', data);
 
 
-// marker size, color, and symbol as an array
-// var trace1 = {
-//     x: [otu_ids],
-//     y: [sample_values],
-//     mode: 'markers',
-//     marker: {
-//       color: [otu_ids],
-//       size: [sample_values],
-//       opacity: [0.6, 0.7, 0.8, 0.9]
-//     },
-//     type: 'scatter',
-//     text: [otu_labels]
-//   };
-  
-//   var trace2 = {
-//     x: [1, 2, 3, 4],
-//     y: [11, 12, 13, 14],
-//     mode: 'markers',
-//     marker: {
-//       color: 'rgb(31, 119, 180)',
-//       size: 18,
-//       symbol: ['circle', 'square', 'diamond', 'cross']
-//     },
-//     type: 'scatter'
-//   };
-  
-//   var trace3 = {
-//     x: [1, 2, 3, 4],
-//     y: [12, 13, 14, 15],
-//     mode: 'markers',
-//     marker: {
-//       size: 18,
-//       line: {
-//         color: ['rgb(120,120,120)', 'rgb(120,120,120)', 'red', 'rgb(120,120,120)'],
-//         width: [2, 2, 6, 2]
-//       }
-//     },
-//     type: 'scatter'
-//   };
-  
-// var data = [trace1, trace2, trace3];
-// var layout = {showlegend: false};
-  
-// Plotly.newPlot('myDiv', data, layout);
-function changeOption(select_data){
-  console.lot(select_data)
+// updates things when something is chosen
+
+function optionChanged(choose_data){
+  console.log(choose_data)
+  create_charts(choose_data)
 
 }
